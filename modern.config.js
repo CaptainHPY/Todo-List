711a7ef8e8a4625b5263edf75815aea57da0f935
CaptainHPY <3438371073@qq.com>
2024-02-18 17:36:38 +0800

    提交文件

--- modern.config.js
+++ modern.config.js
@@ -1,4 +1,5 @@
 import { appTools } from '@modern-js/app-tools';
+import {defineConfig} from '@modern-js/app-tools';
 
 // https://modernjs.dev/en/configure/app/usage
 module.exports = {
@@ -7,3 +8,16 @@ module.exports = {
   },
   plugins: [appTools()],
 };
+
+export default defineConfig ({
+    output: { // 配置静态资源打包路径
+        assetPrefix: '../../'
+    },
+    tools: { // 清除console日志
+        terser: opt => {
+            if (typeof opt.terserOptions.compress === 'object') {
+                opt.terserOptions.compress.drop_console = true;
+              }
+        }
+    }
+})
